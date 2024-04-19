dfefbecab29d40b7e08245f97b7157c1adab19bc
KatalinLengyel <158131186+KatalinLengyel@users.noreply.github.com>
2024-04-19 10:55:37 +0000

    Deploying to gh-pages from @ KatalinLengyel/Ice-cream-project@316503504291cf00e0578c491f8f1630bc9a213a 🚀

---
+++ index.4a65b752.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",pagination:{clickable:!0,el:".testimonial-dots"}});
+//# sourceMappingURL=index.4a65b752.js.map
---
+++ index.a87854c9.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",pagination:{clickable:!0,el:".testimonial-dots"}});
+//# sourceMappingURL=index.a87854c9.js.map
